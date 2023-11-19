b88319bee0606d8a69d817326d666f38bb6b86b9
Ayush Soni <ayush14062004@gmail.com>
2023-11-19 06:21:41 +0530

    NOTIFICATION

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+/* NOTIFICATION START  */
+  const button = document.querySelector('.button');
+const box = document.querySelector('.box');
+let count = 0;
+
+button.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevents the click event from reaching the document
+    if (count % 2 === 0) {
+        box.classList.add("box1");
+        box.classList.remove("box2");
+    } else {
+        box.classList.add("box2");
+        box.classList.remove("box1");
+    }
+    count++;
+});
+
+// Add event listener to document to close the box when clicking outside
+document.addEventListener('click', (event) => {
+    const isClickedInsideButton = button.contains(event.target);
+    const isClickedInsideBox = box.contains(event.target);
+    
+    if (!isClickedInsideButton && !isClickedInsideBox) {
+        box.classList.add("box2");
+        box.classList.remove("box1");
+        count++;
+    }
+});
+
+/* NOTIFICATION END */
